534bf7d4c260c3cfb8c0edc7eebc7a157bf2d36a
Eric Provencher <eprovencher92@gmail.com>
2016-01-20 21:18:59 -0500

    Finished Lab 2

--- Labs/2-MovemementAI/Assets/Seek.cs
+++ Labs/2-MovemementAI/Assets/Seek.cs
@@ -6,13 +6,18 @@ public class Seek : MonoBehaviour
     public Transform target;
     public float moveSpeed;
 
+    public SteeringController control;
+    public Rigidbody rb;
 
     // Use this for initialization
     void Start()
     {
+        control = GetComponent<SteeringController>();
+        rb = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate()
-    {
+    void Update()
+    {    
+        transform.position += moveSpeed/10 *control.seek(target.position)*Time.deltaTime;
     }
 }
