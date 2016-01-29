df174783dab45301c7c0c1ae841dc45871085ab1
Eric Provencher <eprovencher92@gmail.com>
2016-01-29 12:59:33 -0500

    Added L3

---
+++ L3-Flocking/Assets/DroneBehavior.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DroneBehavior : MonoBehaviour {
+
+	// other members of my swarm
+	public List<GameObject> drones;
+	public SwarmBehavior swarm;
+
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		
+	}
+
+	void FixedUpdate()
+	{
+		// we should always apply physics forces in FixedUpdate
+
+	}
+}
+
---
+++ L3-Flocking/Assets/SwarmBehavior.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// An implementation of the flocking algorithm: http://www.red3d.com/cwr/boids/
+// Additional resources:
+// http://harry.me/2011/02/17/neat-algorithms---flocking/
+public class SwarmBehavior : MonoBehaviour {
+	/// <summary>
+	/// the number of drones we want in this swarm
+	/// </summary>
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
