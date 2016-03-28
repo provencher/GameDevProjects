20de787c510346ac24fab5f0a161928f7c928d62
Eric Provencher <eprovencher92@gmail.com>
2016-03-28 16:44:22 -0400

    Destructible terrain

---
+++ A3_Tanks/A3_Tanks/Assets/Scripts/DestroyWall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class DestroyWall : NetworkBehaviour
+{
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        CmdBreakWall();
+
+        if (gameObject.tag == "Bullet")
+        {
+
+        }                      
+    }
+
+    [Command]
+    void CmdBreakWall()
+    {
+        Destroy(gameObject);
+        Pathfinding.instance.grid.CreateGrid();
+    }
+}
